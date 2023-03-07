b0adb6bff24aaff720c8f5998cd34281b184a18f
Zhaozeyuan <zhaozeyuan1103@qq.com>
2023-03-07 21:37:57 +0800

    977.cpp 有序数组的平方（双指针）20230307

--- code/array/977.cpp
+++ code/array/977.cpp
@@ -1,1 +1,38 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    void f(vector<int>& v){
+        int l=0, r = v.size()-1;
+        vector<int> ans(v.size());
+        int cur = ans.size()-1;
+        while(l <= r){
+            if( v[l]*v[l] > v[r]*v[r]){
+                ans[cur--] = v[l]*v[l];
+                l++;
+            }else{
+                ans[cur--] = v[r]*v[r];
+                r--;
+            }
+        }
+        for(auto a : ans) cout << a << ", ";
+    }
+};
+
+int main(){
+    int n;
+    cin>> n;
+    vector<int> vec;
+    for (int i = 0; i < n; ++i) {
+        int elem;
+        cin>>elem;
+        vec.emplace_back(elem);
+    }
+    Solution solution;
+    solution.f(vec);
+
+    return 0;
+}
